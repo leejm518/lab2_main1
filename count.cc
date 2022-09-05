0f5ba216dcd706b0cdcf0561114d79a741cbace5
leejm518 <leejungmin781@gmail.com>
2022-09-05 21:21:14 +0900

    add count.cc

---
+++ count.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "count.hh"
+
+using namespace std;
+
+void count_(int num){
+	for(int i=num;i>0;i--){
+		cout << "" << 11-i << endl;
+	}
+	cout << "Start!!\n2020314008 Jungmin Lee" << endl;
+}
